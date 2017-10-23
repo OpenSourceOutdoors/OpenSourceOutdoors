1091061c280b5adfa50da253449a6b4b6079e7ca
Bryan Goldstein <goldstein.bryan@bcg.com>
2017-10-22 22:58:15 -0400

    add north american chapter

---
+++ chapters/north-america/README.md
@@ -0,0 +1,3 @@
+# North America OpenSource Outdoors
+
+This is a meta chapter to keep track of trips done between chapters.
---
+++ chapters/north-america/trips/201712??-western-ski-trip.js
@@ -0,0 +1,8 @@
+const destinations = require('destinations');
+
+const location = destinations.US.west.rockies.???;
+
+module.exports = {
+  activities: [ location.activities.skiing, location.activities.hotel, location.activities.winterCamping? ],
+  location
+}
